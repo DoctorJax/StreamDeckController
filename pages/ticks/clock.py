1636ca62c73c1086daa064ed98f0a32332183ee7
DoctorJax <jdl2016nc@gmail.com>
2022-12-27 00:30:39 -0500

    want to always see a clock

---
+++ pages/ticks/clock.py
@@ -0,0 +1,25 @@
+from time import localtime, strftime
+
+format = 2
+formats = ["%I:%M", "%H:%M", "%m-%d\n%I:%M:%S"]
+
+def nextTickWait(coords, page, serial) :
+    return 1 #Time until the next tick in seconds
+
+def getKeyState(coords, page, serial, action) : #Runs every tick
+    #print(coords, page, serial, action)
+
+    if action == "clock" :
+        return {"caption": strftime(formats[format], localtime()),
+                "fontSize": 14,
+                "fontColor": "white",
+                "actions": {}}
+
+def keyPress(coords, page, serial) : #Cycle through time formats on keypress
+    global format
+    format += 1
+
+    if format+1 > len(formats) :
+        format = 0
+
+#print("Hello, world!")
