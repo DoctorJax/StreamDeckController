99795b8086cc58b896e3b52671207189fb065ccf
DoctorJax <jdl2016nc@gmail.com>
2023-02-11 15:41:10 -0500

    Apparently it doesn't like when there's a ton of output lol

--- pages/ticks/sinktoggle.py
+++ pages/ticks/sinktoggle.py
@@ -5,7 +5,7 @@ from PIL import Image
 icon = "./pages/imgs/example/white.jpg"
 
 def audioStatus():
-    output = subprocess.getoutput(r'pactl get-default-sink | grep -Eo "CORSAIR|Dell"')
+    output = subprocess.getoutput(r'pactl get-default-sink | grep -Eo "CORSAIR|Dell" | head -1')
     if output == "CORSAIR":
         icon = "./pages/imgs/headphones.png"
     elif output == "Dell":
@@ -15,7 +15,7 @@ def audioStatus():
     return icon
 
 def batteryStatus():
-    output = subprocess.getoutput(r'pactl get-default-sink | grep -Eo "CORSAIR|Dell"')
+    output = subprocess.getoutput(r'pactl get-default-sink | grep -Eo "CORSAIR"')
     if output == "CORSAIR":
         battery = subprocess.getoutput(r"headsetcontrol -b | grep Battery | awk -F ' ' '{ print $2 }'")
         if battery == "Unavailable":
@@ -41,7 +41,7 @@ def getKeyState(coords, page, serial, action):
 
 def keyPress(coords, page, serial):
     icon = audioStatus()
-    output = subprocess.getoutput(r'pactl get-default-sink | grep -Eo "CORSAIR|Dell"')
+    output = subprocess.getoutput(r'pactl list sinks | grep -Eo "Dell" | head -1')
 
     if output == "Dell":
         subprocess.Popen('/home/jackson/.scripts/audioswap.sh -s', shell=True)
